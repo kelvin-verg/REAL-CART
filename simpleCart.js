556f311b85e04f8ba1f7643f1755ca620033c527
kelvin-verg <kelvinverg420@gmail.com>
2020-01-10 11:41:50 +0300

    new

---
+++ simpleCart.js
@@ -0,0 +1,19 @@
+(function (window, document) {
+	/*global HTMLElement */
+
+	var typeof_string			= typeof "",
+		typeof_undefined		= typeof undefined,
+		typeof_function			= typeof function () {},
+		typeof_object			= typeof {},
+		isTypeOf				= function (item, type) { return typeof item === type; },
+		isString				= function (item) { return isTypeOf(item, typeof_string); },
+		isUndefined				= function (item) { return isTypeOf(item, typeof_undefined); },
+		isFunction				= function (item) { return isTypeOf(item, typeof_function); },
+
+		isObject				= function (item) { return isTypeOf(item, typeof_object); },
+		//Returns true if it is a DOM element
+		isElement				= function (o) {
+			return typeof HTMLElement === "object" ? o instanceof HTMLElement : typeof o === "object" && o.nodeType === 1 && typeof o.nodeName === "string";
+		},
+
+
